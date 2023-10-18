bb1d8910fcd0be86faf6577c1b4536a9bd4a9658
Vishnu Singh <115409013+vishnusingh-12@users.noreply.github.com>
2023-10-19 00:04:10 +0530

    Added the Metropolis Algorithm , added comments

--- main.py
+++ main.py
@@ -1,75 +1,150 @@
-import matplotlib.pyplot as plt
+# importing dependencies
 import numpy as np
 from lattice import Lattice
 import random
 from msd import get_msd
+from scipy.constants import Boltzmann
+import time
+import winsound
 
-mc_steps = 10000
-moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]  # movement of ions up down left right
+# for estimation of simulation run time
+start_time = time.time()
+
+# number of Monte Carlo Steps
+mc_steps = 100000
+
+# Temperature
+temperature = 1300
+
+# Energy interaction between ions
+epsilon = 2223 * Boltzmann
+
+# inverse temperature
+beta = 1 / (Boltzmann * temperature)
+
+# movement of ions up down left right
 # (1,0)=down,(0,1)=right,(-1,0)=up,(0,-1)=left
-for s in [0.9, 0.7, 0.5, 0.3, 0.1]:
-    lattice = Lattice(20, s)
-    lat_2d = lattice.get_lattice()
-    for i in range(mc_steps):
-        ions = lattice.get_ions(lat_2d)
-        np.random.shuffle(ions)
-        # random_ion = random.randint(0, len(ions) - 1)
-        # ions = ions[random_ion:] + ions[:random_ion]
-        for ion in ions:
-            # print('before lattice')
-            # lattice.print_lattice(lat_2d, 'index')
-            # print('selecting ion ', ion.index)
-            # print('before pos ', ion.position)
-            # print('before mapped pos ', ion.mapped_position)
+moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-            step = random.choice(moves)
-            # print('move ', step)
-            potential_pos = [(ion.mapped_position[0] + step[0]) % lattice.size,
-                             (ion.mapped_position[1] + step[1]) % lattice.size]
-            # print('potential position', potential_pos)
-            if lat_2d[potential_pos[0], potential_pos[1]].value == 0:
-                # print('move accepted')
+# Creating Lattice Class Object of 20x20, vacancy, epsilon= interaction term
+lattice = Lattice(20, 0.1, epsilon)
+
+# getting matrix of ions using Lattice class function
+lat_2d = lattice.get_lattice()
+
+# Simulation starts here
+for i in range(mc_steps):
+
+    # for each MC step a list of all ions is fetched from the lattice matrix
+    ions = lattice.get_ions(lat_2d)
+
+    # The list is shuffled so that ions are picked at random
+    np.random.shuffle(ions)
+
+    for ion in ions:
+
+        # for each ion a random step is selected from moves defined above
+        step = random.choice(moves)
+
+        # the potential position of the ion in the lattice is calculated if the move is accepted according to PBC
+        potential_pos = [(ion.mapped_position[0] + step[0]) % lattice.size,
+                         (ion.mapped_position[1] + step[1]) % lattice.size]
+
+        # if an ion is found at potential position
+        if lat_2d[potential_pos[0], potential_pos[1]].value == 1:
+
+            # reject move
+
+            # checks whether to store the ion position or not. I am storing ion position after every 100 steps
+            if (i + 1) % 100 == 0:
+
+                # stores current position of the ion in space into a list
+                ion.position_list.append(ion.position)
+
+            # continue without updating ion as collision has occurred
+            continue
+
+        # if ion not found at potential position
+        else:
+
+            # calculate delta_e between new position and initial position
+            delta_e = lattice.delta_e(ion.mapped_position, potential_pos, lat_2d)
+
+            # if delta e is less than 0
+            if delta_e < 0:
+
+                # always accept the move
+
+                # swap the ion and the vacancy
                 lat_2d[ion.mapped_position[0], ion.mapped_position[1]], lat_2d[potential_pos[0], potential_pos[1]] = \
                     lat_2d[potential_pos[0], potential_pos[1]], lat_2d[ion.mapped_position[0], ion.mapped_position[1]]
+
+                # update the ion position in actual space
                 ion.position = [ion.position[0] + step[0], ion.position[1] + step[1]]
+
+                # update the ion position on the matrix
                 ion.mapped_position = potential_pos
-                ion.position_list.append(ion.position)
-                # print('after pos ', ion.position)
-                # print('after mapped pos ', ion.mapped_position)
-                # print('after lattice')
-                # lattice.print_lattice(lat_2d, 'index')
-                # print(ion.position_list)
+
+                # checks whether to store the ion position or not. I am storing ion position after every 100 steps
+                if (i + 1) % 100 == 0:
+
+                    # stores current position of the ion in space into a list
+                    ion.position_list.append(ion.position)
+
+            # if delta e is greater than 0
             else:
-                # print('move rejected')
-                ion.position_list.append(ion.position)
-                # print('after pos ', ion.position)
-                # print('after mapped pos ', ion.mapped_position)
-                # print('after lattice')
-                # lattice.print_lattice(lat_2d, 'index')
-                # print(ion.position_list)
-                continue
-    ions = lattice.get_ions(lat_2d)
-    # x = [i[1] for i in ions[0].position_list]
-    # y = [i[0] for i in ions[0].position_list]
-    # x1 = [i[1] for i in ions[1].position_list]
-    # y1 = [i[0] for i in ions[1].position_list]
-    # x2 = [i[1] for i in ions[2].position_list]
-    # y2 = [i[0] for i in ions[2].position_list]
-    # x3= [i[1] for i in ions[-1].position_list]
-    # y3 = [i[0] for i in ions[-1].position_list]
-    # x4= [i[1] for i in ions[5].position_list]
-    # y4 = [i[0] for i in ions[5].position_list]
-    # plt.plot(x, y)
-    # plt.plot(x1, y1)
-    # plt.plot(x2, y2)
-    # plt.plot(x3, y3)
-    # plt.plot(x4, y4)
-    # plt.show()
-    msd = get_msd(ions, mc_steps // 2)
 
-    # print(msd)
+                # generate a random number between 0 and 1 and compare exp(-beta*epsilon)
 
-    # print(msd_new == msd)
+                # if random number is less than or equal to exp(-beta*epsilon)
+                if random.random() <= np.exp(-beta * delta_e):
+                    # accept the move
 
-    np.save(f'vacancy_{s * 100}.npy', msd)
-    print(s)
+                    # swap the ion and the vacancy
+                    lat_2d[ion.mapped_position[0], ion.mapped_position[1]], lat_2d[potential_pos[0], potential_pos[1]] = \
+                        lat_2d[potential_pos[0], potential_pos[1]], lat_2d[
+                            ion.mapped_position[0], ion.mapped_position[1]]
+
+                    # update the ion position in actual space
+                    ion.position = [ion.position[0] + step[0], ion.position[1] + step[1]]
+
+                    # update the ion position on the matrix
+                    ion.mapped_position = potential_pos
+
+                    # checks whether to store the ion position or not. I am storing ion position after every 100 steps
+                    if (i + 1) % 100 == 0:
+
+                        # stores current position of the ion in space into a list
+                        ion.position_list.append(ion.position)
+
+                # if random number is greater than exp(-beta*epsilon)
+                else:
+
+                    # reject move
+
+                    # checks whether to store the ion position or not. I am storing ion position after every 100 steps
+                    if (i + 1) % 100 == 0:
+
+                        # stores current position of the ion in space into a list
+                        ion.position_list.append(ion.position)
+
+                    # continue without updating ion as collision has occurred
+                    continue
+
+# again getting ions list after simulation run
+ions = lattice.get_ions(lat_2d)
+
+# calculating msd , parameters passed are ions list and half of number of positions captured
+msd = get_msd(ions, 500)  # original msd calculation
+
+# saving msd to file
+np.save(f'vacancy_{10}_metro.npy', msd)
+
+# for estimation of simulation run time
+end_time = time.time()
+
+# prints total time of simulation run in seconds
+print(end_time - start_time)
+
+# tells me when the simulation run is complete
+winsound.Beep(1000, 500)
